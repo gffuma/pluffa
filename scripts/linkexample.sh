dd4f27eb84a70284b62d5efd931c17a8000095c0
gffuma <fumagalli.gf@gmail.com>
2022-01-30 10:54:02 +0100

    Hack n fixies

--- scripts/linkexample.sh
+++ scripts/linkexample.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-EX=base
+# Is this a workaround? I live using workarounds since '93
+
+EX=${1:-base}
 
 rm -rf ./node_modules/react
 rm -rf ./node_modules/react-dom
 ln -s ../examples/${EX}/node_modules/react node_modules/react
 ln -s ../examples/${EX}/node_modules/react-dom node_modules/react-dom
+cd ./examples/${EX} && yarn link snext && cd ../../
--- src/build.ts
+++ src/build.ts
@@ -59,6 +59,32 @@ export default function build({
             ],
           },
           {
+            test: /\.svg$/,
+            use: [
+              {
+                loader: require.resolve('@svgr/webpack'),
+                options: {
+                  prettier: false,
+                  svgo: false,
+                  svgoConfig: {
+                    plugins: [{ removeViewBox: false }],
+                  },
+                  titleProp: true,
+                  ref: true,
+                },
+              },
+              {
+                loader: require.resolve('file-loader'),
+                options: {
+                  name: 'static/media/[name].[hash].[ext]',
+                },
+              },
+            ],
+            issuer: {
+              and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+            },
+          },
+          {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
             type: 'asset',
             parser: {
@@ -152,6 +178,33 @@ export default function build({
               modules: {
                 exportOnlyLocals: true,
               },
+            },
+          },
+          {
+            test: /\.svg$/,
+            use: [
+              {
+                loader: require.resolve('@svgr/webpack'),
+                options: {
+                  prettier: false,
+                  svgo: false,
+                  svgoConfig: {
+                    plugins: [{ removeViewBox: false }],
+                  },
+                  titleProp: true,
+                  ref: true,
+                },
+              },
+              {
+                loader: require.resolve('file-loader'),
+                options: {
+                  emitFile: false,
+                  name: 'static/media/[name].[hash].[ext]',
+                },
+              },
+            ],
+            issuer: {
+              and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
             },
           },
           {
