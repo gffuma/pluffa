3e2ecb3462788efbebdccc4aa8237918c846ff46
gffuma <fumagalli.gf@gmail.com>
2022-10-07 12:43:31 +0200

    Allow  in schema lol

--- schema.ts
+++ schema.ts
@@ -1,4 +1,9 @@
 import type { NodeConfig } from '@pluffa/node/config'
 import type { CloudFlareWorkersConfig } from '@pluffa/cloudflare-workers/config'
 
-export type Config = NodeConfig | CloudFlareWorkersConfig
+// Allow meta "$schema" key in pluffa.json
+interface OptionalSchemaURL {
+  $schema?: string
+}
+
+export type Config = (NodeConfig | CloudFlareWorkersConfig) & OptionalSchemaURL
